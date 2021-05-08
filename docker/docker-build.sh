1fe9f35db3a24ddfc77fa1319a17e4dc4c405299
Ruud van Vliet <ruud.van.vliet@trivento.nl>
2021-05-08 19:41:40 +0200

    favicon.ico + opschonen

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mkdir -p ./www
-cp ../index.* ./www
+cp ../index.* ../favicon.ico ./www
 docker build  -t rimvanvliet/mondriaan-klok:latest .
 # docker push rimvanvliet/mondriaan-klok:latest
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 const klokKleur = ['white','yellow','red','blue']
 
 const getalNamen = ["half","een","twee","drie","vier","vijf","zes","zeven","acht","negen","tien","elf",
 					"twaalf","dertien","veertien","kwart","zestien","zeventien","achttien","negentien"]
 
-const pad = (num) => { return ('00000' + num).substr(-6); }
+const pad6 = (num) => { return ('00000' + num).substr(-6); }
 
 const refresh = () => {
 
@@ -14,15 +13,13 @@ const refresh = () => {
 	const minuutNum = now.getMinutes()
 	const uurNum = now.getHours()
 
-	const uren = pad(uurNum.toString(2)).split("").map(x => 2 * x)
-	const minuten = pad(minuutNum.toString(2)).split("").map(x => parseInt(x))
+	const uren = pad6(uurNum.toString(2)).split("").map(x => 2 * x)
+	const minuten = pad6(minuutNum.toString(2)).split("").map(x => parseInt(x))
 	const kleurNrs = uren.map(function(u, i) { return u + minuten[i]})
 
 	kleurNrs.forEach(function(kleurNr, i) {
 		$('#'+i).css('background-color', klokKleur[kleurNr]);
 	})
-
-	setTimeout(refresh, 1000*(60 - now.getSeconds()) - now.getMilliseconds());
 
 	const minuutNaam =  (minuutNum === 0) ? "" :
 						(minuutNum <= 19) ? getalNamen[minuutNum] + "<br>over<br>" :
@@ -33,8 +30,9 @@ const refresh = () => {
 	const uurNaam =     (minuutNum === 0) ? getalNamen[uurNum % 12] + "<br>uur" :
 						(minuutNum <= 19) ? getalNamen[uurNum % 12] : getalNamen[(uurNum % 12) + 1]
 	$('p').html("het<br>is<br>"+minuutNaam+uurNaam);
-	console.log("het<br>is<br>"+minuutNaam+uurNaam)
+	console.log("het is "+(minuutNaam+uurNaam).replaceAll("<br>", " "))
 
+	setTimeout(refresh, 1000*(60 - now.getSeconds()) - now.getMilliseconds());
 }
 
 refresh()
