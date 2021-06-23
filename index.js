9d00b46af9732ded41106d17109fc4f6a11f082f
Ruud van Vliet <ruud.van.vliet@trivento.nl>
2021-06-23 19:24:26 +0200

    lijnen dunner gemaakt js opgeschoond vanaf min > 40 voor het uur

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const pad6 = (num) => { return ('00000' + num).substr(-6); }
 const refresh = () => {
 
 	const now = new Date();
-	console.log(now)
-
 	const minuutNum = now.getMinutes()
 	const uurNum = now.getHours()
 
 	const uren = pad6(uurNum.toString(2)).split("").map(x => 2 * x)
-	const minuten = pad6(minuutNum.toString(2)).split("").map(x => parseInt(x))
+	const minuten = pad6(minuutNum.toString(2)).split("").map(x => 1 * x)
 	const kleurNrs = uren.map(function(u, i) { return u + minuten[i]})
 
 	kleurNrs.forEach(function(kleurNr, i) {
@@ -25,13 +23,12 @@ const refresh = () => {
 						(minuutNum <= 19) ? getalNamen[minuutNum] + "<br>over<br>" :
 						(minuutNum <= 29) ? getalNamen[30 - minuutNum] + "<br>voor<br>half<br>" :
 						(minuutNum === 30) ? "half<br>" :
-						(minuutNum <= 44) ? getalNamen[minuutNum - 30] + "<br>over<br>half<br>" :
+						(minuutNum <= 40) ? getalNamen[minuutNum - 30] + "<br>over<br>half<br>" :
 						getalNamen[60 - minuutNum] + "<br>voor<br>"
 
 	const uurNaam =     (minuutNum === 0) ? getalNamen[uurNum % 12] + "<br>uur" :
 						(minuutNum <= 19) ? getalNamen[uurNum % 12] : getalNamen[(uurNum % 12 + 1) ]
 	$('.tijdtekst').html("het<br>is<br>"+minuutNaam+uurNaam);
-	console.log("het is "+(minuutNaam+uurNaam).replaceAll("<br>", " "))
 
 	setTimeout(refresh, 1000*(60 - now.getSeconds()) - now.getMilliseconds());
 }
